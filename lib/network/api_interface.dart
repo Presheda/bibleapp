9d727fdc0c780e878416a87c5caec249eecbe64d
Edafe Precious <preciousedafe15@gmail.com>
2023-06-02 09:51:00 +0100

    added typedef, added api_interface

---
+++ lib/network/api_interface.dart
@@ -0,0 +1,32 @@
+import 'package:bibleapp/helpers/typedefs.dart';
+import 'package:dio/dio.dart';
+
+import 'response_model.dart';
+
+abstract class ApiInterface {
+  const ApiInterface();
+
+  Future<List<T>> getCollectionData<T>({
+    required String endpoint,
+    JSON? queryParams,
+    CancelToken? cancelToken,
+    bool requiresAuthToken = true,
+    required T Function(JSON responseBody) converter,
+  });
+
+  Future<T> getDocumentData<T>({
+    required String endpoint,
+    JSON? queryParams,
+    CancelToken? cancelToken,
+    bool requiresAuthToken = true,
+    required T Function(JSON responseBody) converter,
+  });
+
+  Future<T> setData<T>({
+    required String endpoint,
+    required JSON data,
+    CancelToken? cancelToken,
+    bool requiresAuthToken = true,
+    required T Function(ResponseModel<JSON> response) converter,
+  });
+}
---
+++ lib/network/response_model.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:bibleapp/helpers/typedefs.dart';
+
+class ResponseModel<T> {
+  final _ResponseModelHeadersModel headers;
+  final T body;
+
+  const ResponseModel({required this.headers, required this.body});
+
+  factory ResponseModel.fromJson(JSON json) {
+    return ResponseModel(
+        headers: _ResponseModelHeadersModel.fromJson(json['headers'] as JSON),
+        body: json['body'] as T);
+  }
+}
+
+class _ResponseModelHeadersModel {
+  final bool error;
+  final String message;
+  final String? code;
+
+  const _ResponseModelHeadersModel(
+      {required this.error, required this.message, this.code});
+
+  factory _ResponseModelHeadersModel.fromJson(JSON json) {
+    return _ResponseModelHeadersModel(
+        error: (json['error'] as int) == 1,
+        message: json['message'] as String,
+        code: json['code'] as String);
+  }
+}
